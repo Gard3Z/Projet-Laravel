a216f87baa1024f640c11be2ecedff139a5fdcc0
Yoann <lb.yoann16@gmail.com>
2024-02-08 17:03:45 +0100

    deploy completed

--- app/Envoy.blade.php
+++ app/Envoy.blade.php
@@ -1,10 +1,15 @@
 @servers(['production' => ['yoann-le-bihan@13.39.150.46']])
- 
+
 @task('deploy', ['on' => 'production'])
     cd yoann-le-bihan.dhonnabhain.me
     git checkout production
     cd app
     composer update
-    php artisan migrate
+    npm install
+    npm run build
+    php artisan migrate --force
     composer install --optimize-autoloader --no-dev
+    php artisan optimize:clear
+    php artisan optimize
+    php artisan key:generate
 @endtask
